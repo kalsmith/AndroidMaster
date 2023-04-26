b1926db6d514a0ecde99bd2652f0e837e3e43b95
Cesar.Labarca <nsuversivo@yahoo.es>
2023-04-26 17:33:31 -0400

    Se agregaron las task

---
+++ app/src/main/java/com/kal/androidmaster/firstapp/todoapp/TaskViewHolder.kt
@@ -0,0 +1,20 @@
+package com.kal.androidmaster.firstapp.todoapp
+
+import android.view.View
+import android.widget.CheckBox
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.kal.androidmaster.R
+
+class TaskViewHolder(view: View) : RecyclerView.ViewHolder(view){
+
+    private val tvTask:TextView = view.findViewById(R.id.tvTask)
+    private val cbTask:CheckBox = view.findViewById(R.id.cbTask)
+
+
+    fun render(task:Task){
+        tvTask.text = task.name
+
+    }
+
+}
--- app/src/main/java/com/kal/androidmaster/firstapp/todoapp/TodoActivity.kt
+++ app/src/main/java/com/kal/androidmaster/firstapp/todoapp/TodoActivity.kt
@@ -5,8 +5,7 @@ import android.os.Bundle
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.kal.androidmaster.R
-
-
+import com.kal.androidmaster.firstapp.todoapp.TaskCategory.*
 
 
 class TodoActivity : AppCompatActivity() {
@@ -14,12 +13,20 @@ class TodoActivity : AppCompatActivity() {
     private lateinit var rvCategories:RecyclerView
     private lateinit var categoriesAdapter: CategoryAdapter
 
+    private lateinit var rvTask: RecyclerView
+    private lateinit var taskAdapter: TaskAdapter
+
     private val categories = listOf(
-        TaskCategory.Business,
-        TaskCategory.Personal,
-        TaskCategory.Other
+        Business,
+        Personal,
+        Other
     )
 
+    private val tasks = mutableListOf(
+        Task("PRUEBA ASDT", Business),
+        Task("PRUEBA PERSONA", Personal),
+        Task("PRUEBA reeeeeeee", Other)
+    )
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,11 +38,16 @@ class TodoActivity : AppCompatActivity() {
 
     private fun initComponent() {
         rvCategories = findViewById(R.id.rvCategories)
+        rvTask = findViewById(R.id.rvTask)
     }
 
     private fun initUI() {
         categoriesAdapter = CategoryAdapter(categories)
         rvCategories.layoutManager = LinearLayoutManager(this,LinearLayoutManager.HORIZONTAL, false)
         rvCategories.adapter = categoriesAdapter
+
+        taskAdapter = TaskAdapter(tasks)
+        rvTask.layoutManager = LinearLayoutManager(this)
+        rvTask.adapter = taskAdapter
     }
 }
