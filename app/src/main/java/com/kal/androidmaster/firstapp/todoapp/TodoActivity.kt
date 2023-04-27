bce97dec8072880d431cc7b73943085293e3986b
KAL <nsuversivo@yahoo.es>
2023-04-26 23:39:07 -0400

    Se agrega radio button y float view

--- app/src/main/java/com/kal/androidmaster/firstapp/todoapp/TodoActivity.kt
+++ app/src/main/java/com/kal/androidmaster/firstapp/todoapp/TodoActivity.kt
@@ -1,9 +1,11 @@
 package com.kal.androidmaster.firstapp.todoapp
 
+import android.app.Dialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.kal.androidmaster.R
 import com.kal.androidmaster.firstapp.todoapp.TaskCategory.*
 
@@ -16,6 +18,8 @@ class TodoActivity : AppCompatActivity() {
     private lateinit var rvTask: RecyclerView
     private lateinit var taskAdapter: TaskAdapter
 
+    private lateinit var fabAddTask: FloatingActionButton
+
     private val categories = listOf(
         Business,
         Personal,
@@ -23,9 +27,9 @@ class TodoActivity : AppCompatActivity() {
     )
 
     private val tasks = mutableListOf(
-        Task("PRUEBA ASDT", Business),
-        Task("PRUEBA PERSONA", Personal),
-        Task("PRUEBA reeeeeeee", Other)
+        Task("PRUEBA NEGOCIOS", Business),
+        Task("PRUEBA PERSONAL", Personal),
+        Task("PRUEBA OTROS", Other)
     )
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -33,12 +37,24 @@ class TodoActivity : AppCompatActivity() {
         setContentView(R.layout.activity_todo)
         initComponent()
         initUI()
+        initListener()
 
+    }
+
+    private fun initListener() {
+        fabAddTask.setOnClickListener{ showDialog()}
+    }
+
+    private fun showDialog() {
+        val dialog = Dialog(this)
+        dialog.setContentView(R.layout.dialog_task)
+        dialog.show()
     }
 
     private fun initComponent() {
         rvCategories = findViewById(R.id.rvCategories)
         rvTask = findViewById(R.id.rvTask)
+        fabAddTask = findViewById(R.id.fabAddTask)
     }
 
     private fun initUI() {
