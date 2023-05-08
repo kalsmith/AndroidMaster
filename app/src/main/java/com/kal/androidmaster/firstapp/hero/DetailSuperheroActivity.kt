e50d36717efdb1d778308c56cfbce716d0a38764
KAL <nsuversivo@yahoo.es>
2023-05-07 23:01:45 -0400

    TERMINO DE APP CON CONSUMO DE API

--- app/src/main/java/com/kal/androidmaster/firstapp/hero/DetailSuperheroActivity.kt
+++ app/src/main/java/com/kal/androidmaster/firstapp/hero/DetailSuperheroActivity.kt
@@ -2,6 +2,8 @@ package com.kal.androidmaster.firstapp.hero
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.TypedValue
+import android.view.View
 import com.kal.androidmaster.R
 import com.kal.androidmaster.databinding.ActivityDetailSuperHeroBinding
 import com.squareup.picasso.Picasso
@@ -10,6 +12,7 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import kotlin.math.roundToInt
 
 class DetailSuperheroActivity : AppCompatActivity() {
 
@@ -43,7 +46,31 @@ class DetailSuperheroActivity : AppCompatActivity() {
     private fun createUI(superhero: SuperHeroDetailResponse) {
         Picasso.get().load(superhero.image.url).into(binding.ivSuperhero)
         binding.tvSuperheroName.text = superhero.name
+        prepareStats(superhero.powerstats)
+        binding.tvSuperRealName.text = superhero.biography.fullName
+        binding.tvPublisher.text = superhero.biography.publisher
+
     }
+
+    private fun prepareStats(powerstats: PowerStatsResponse) {
+        updateHeight(binding.viewCombat, powerstats.combat)
+        updateHeight(binding.viewDurability, powerstats.durability)
+        updateHeight(binding.viewSpeed, powerstats.speed)
+        updateHeight(binding.viewStrength, powerstats.strength)
+        updateHeight(binding.viewIntelligence, powerstats.intelligence)
+        updateHeight(binding.viewPower, powerstats.power)
+    }
+
+    private fun updateHeight(view: View, stat:String){
+        val params = view.layoutParams
+        params.height = pxToDp(stat.toFloat())
+        view.layoutParams = params
+    }
+
+    private fun pxToDp(px: Float): Int {
+        return TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, px, resources.displayMetrics).roundToInt()
+    }
+
 
     private fun getRetrofit(): Retrofit{
         return Retrofit
--- app/src/main/java/com/kal/androidmaster/firstapp/hero/SuperHeroDetailResponse.kt
+++ app/src/main/java/com/kal/androidmaster/firstapp/hero/SuperHeroDetailResponse.kt
@@ -5,7 +5,8 @@ import com.google.gson.annotations.SerializedName
 data class SuperHeroDetailResponse(
     @SerializedName("name") val name: String,
     @SerializedName("powerstats") val powerstats: PowerStatsResponse,
-    @SerializedName("image") val image: SuperheroImageDetailResponse
+    @SerializedName("image") val image: SuperheroImageDetailResponse,
+    @SerializedName("biography") val biography: Biography
 )
 data class PowerStatsResponse(
     @SerializedName("intelligence") val intelligence: String,
@@ -19,3 +20,9 @@ data class PowerStatsResponse(
 )
 
 data class SuperheroImageDetailResponse(@SerializedName("url") val url:String)
+
+data class Biography(
+    @SerializedName("full-name") val fullName:String,
+    @SerializedName("publisher") val publisher:String
+
+    )
